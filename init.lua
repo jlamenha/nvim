f739d1eacdfdc9127b30e0902cdef4093162ff23
minaboktor2628 <mina26b8@gmail.com>
2024-09-08 15:42:18 -0400

    debugger

--- init.lua
+++ init.lua
@@ -131,6 +131,11 @@ vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
 
+vim.opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.softtabstop = 2 -- Number of spaces that a <Tab> counts for while editing
+
 -- Enable break indent
 vim.opt.breakindent = true
 
@@ -288,6 +293,49 @@ require('lazy').setup({
     },
   },
 
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'theHamsta/nvim-dap-virtual-text',
+      'williamboman/mason.nvim',
+    },
+    config = function()
+      local dap = require 'dap'
+      local ui = require 'dapui'
+
+      require('dapui').setup()
+      require('nvim-dap-virtual-text').setup()
+
+      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = '[B]reak points' })
+
+      -- Eval var under cursor
+      vim.keymap.set('n', '<leader>?', function()
+        require('dapui').eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set('n', '<F1>', dap.continue)
+      vim.keymap.set('n', '<F2>', dap.step_into)
+      vim.keymap.set('n', '<F3>', dap.step_over)
+      vim.keymap.set('n', '<F4>', dap.step_out)
+      vim.keymap.set('n', '<F5>', dap.step_back)
+      vim.keymap.set('n', '<F12>', dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
   -- file explorer for nvim: <leader>o
   {
     'nvim-tree/nvim-tree.lua',
@@ -751,7 +799,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        typescript = { 'prettier' },
+        typescript = { 'prettier', 'prettier' },
         c = { 'clang-format' },
         cpp = { 'clang-format' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
