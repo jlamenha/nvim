1f268a8266dd80bed55d244f341cb8f930d4e0d8
jlamenha <lamenhajoaolucas@gmail.com>
2024-12-02 20:39:27 +0100

    fixed stuff

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,12 +7,15 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
+    ft = 'rust',
     config = function()
       local mason_registry = require 'mason-registry'
       local codelldb = mason_registry.get_package 'codelldb'
       local extension_path = codelldb:get_install_path() .. '/extension/'
       local codelldb_path = extension_path .. 'adapter/codelldb'
-      local liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
+      local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
+      -- If you are on Linux, replace the line above with the line below:
+      -- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
       local cfg = require 'rustaceanvim.config'
 
       vim.g.rustaceanvim = {
@@ -23,7 +26,14 @@ return {
     end,
   },
   {
-    'mfussneger/nvim-dap',
+    'rust-lang/rust.vim',
+    ft = 'rust',
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    'mfussenegger/nvim-dap',
     config = function()
       local dap, dapui = require 'dap', require 'dapui'
       dap.listeners.before.attach.dapui_config = function()
@@ -42,7 +52,7 @@ return {
   },
   {
     'rcarriga/nvim-dap-ui',
-    dependencies = { 'mfussneger/nvim-dap', 'nvim-neotest/nvim-nio' },
+    dependencies = { 'mfusseneger/nvim-dap', 'nvim-neotest/nvim-nio' },
     config = function()
       require('dapui').setup()
     end,
