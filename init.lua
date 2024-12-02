fae04bdee02b1a315c6d7e25738488c7c44a1a52
jlamenha <lamenhajoaolucas@gmail.com>
2024-12-02 18:53:12 +0100

    added Rust support

--- init.lua
+++ init.lua
@@ -256,6 +256,24 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Nvim DAP
+vim.keymap.set('n', '<Leader>dl', "<cmd>lua require'dap'.step_into()<CR>", { desc = 'Debugger step into' })
+vim.keymap.set('n', '<Leader>dj', "<cmd>lua require'dap'.step_over()<CR>", { desc = 'Debugger step over' })
+vim.keymap.set('n', '<Leader>dk', "<cmd>lua require'dap'.step_out()<CR>", { desc = 'Debugger step out' })
+vim.keymap.set('n', '<Leader>dc', "<cmd>lua require'dap'.continue()<CR>", { desc = 'Debugger continue' })
+vim.keymap.set('n', '<Leader>db', "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = 'Debugger toggle breakpoint' })
+vim.keymap.set(
+  'n',
+  '<Leader>dd',
+  "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+  { desc = 'Debugger set conditional breakpoint' }
+)
+vim.keymap.set('n', '<Leader>de', "<cmd>lua require'dap'.terminate()<CR>", { desc = 'Debugger reset' })
+vim.keymap.set('n', '<Leader>dr', "<cmd>lua require'dap'.run_last()<CR>", { desc = 'Debugger run last' })
+
+-- rustaceanvim
+vim.keymap.set('n', '<Leader>dt', "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = 'Debugger testables' })
+
 -- spelling error
 -- vim.api.nvim_set_hl(0, 'SpellBad', { undercurl = true, sp = 'green' })
 -- Enable spell checking globally for all file types
